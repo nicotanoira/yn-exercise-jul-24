4a69d70868b2a1cceba78e8e1d85a3d08146e58f
ntanoiralatech <nicolastanoira@latechfactory.com>
2024-07-11 15:02:14 -0300

    Feat: useReset hook TASK 6

--- src/api-hooks/useResetAnswers.ts
+++ src/api-hooks/useResetAnswers.ts
@@ -8,4 +8,16 @@ import { useAnswersStore } from '../state'
 // - You need to implement a new hook called useResetAnswers.
 // - Once implemented, you should be able to use this hook in the Table view.
 
-export const useResetAnswers = () => {}
+export const useResetAnswers = () => {
+    const setAnswers = useAnswersStore(state => state.setAnswers)
+
+    return useMutation(resetAnswersFromApi, {
+        onSuccess: data => {
+            const domainAnswers = apiToDomainAnswersConverter(data.data)
+            setAnswers(domainAnswers)
+        },
+        onError: error => {
+            console.error('Failed to reset answers', error)
+        },
+    })
+}
